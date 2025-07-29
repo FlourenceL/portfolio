2ea6ee0fe49ab9c58b1123862c3f7962aea8edb7
Flourence <renzo.lapore@gmail.com>
2025-07-29 17:24:28 +0800

    Updated the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import { Navbar } from "@/components/views/landing-page/navbar";
+import Navbar01Page from "@/components/views/navbar-01/navbar-01";
 import PortfolioHeroSplitLayout from "@/components/views/landing-page/hero";
-import Logos06Page from "@/components/logos-06/logos-06";
+import Logos06Page from "@/components/views/landing-page/logos-06/logos-06";
 import SkillsDisplay from "@/components/views/landing-page/skills-display";
 import PortfolioProjectStandardGrid from "@/components/views/landing-page/projects-display";
-import Timeline from "@/components/timeline-01/timeline-01";
+import Timeline from "@/components/views/timeline-01/timeline-01";
 import { Contact2 } from "@/components/views/landing-page/contact2";
 import FooterSimple from "@/components/views/landing-page/footer";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      <Navbar01Page />
       <PortfolioHeroSplitLayout />
       <Logos06Page />
       <SkillsDisplay />
