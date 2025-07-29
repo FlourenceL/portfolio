6feec2e418ad7a992710726c8d69fafc530310a8
Flourence <renzo.lapore@gmail.com>
2025-07-29 10:42:05 +0800

    first commit

---
+++ src/components/views/landing-page/navbar.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const Navbar = () => {
+  return (
+    <section className="pt-5 px-5">
+      <div className="container">
+        <nav className="flex items-center justify-between">
+          <a
+            href="https://www.shadcnblocks.com"
+            className="flex items-center gap-2"
+          >
+            <img
+              src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-icon.svg"
+              className="max-h-8"
+              alt="Shadcn UI Navbar"
+            />
+            <span className="text-lg font-semibold tracking-tighter">
+              Shadcnblocks.com
+            </span>
+          </a>
+          <NavigationMenu className="hidden lg:block">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Products
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Resources
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="#"
+                  className={navigationMenuTriggerStyle()}
+                >
+                  Contact
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="hidden items-center gap-4 lg:flex">
+            <Button variant="outline">Sign in</Button>
+            <Button>Start for free</Button>
+          </div>
+          <Sheet>
+            <SheetTrigger asChild className="lg:hidden">
+              <Button variant="outline" size="icon">
+                <MenuIcon className="h-4 w-4" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="top" className="max-h-screen overflow-auto">
+              <SheetHeader>
+                <SheetTitle>
+                  <a
+                    href="https://www.shadcnblocks.com"
+                    className="flex items-center gap-2"
+                  >
+                    <img
+                      src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-icon.svg"
+                      className="max-h-8"
+                      alt="Shadcn UI Navbar"
+                    />
+                    <span className="text-lg font-semibold tracking-tighter">
+                      Shadcnblocks.com
+                    </span>
+                  </a>
+                </SheetTitle>
+              </SheetHeader>
+              <div className="flex flex-col p-4">
+                <div className="flex flex-col gap-6">
+                  <a href="#" className="font-medium">
+                    Templates
+                  </a>
+                  <a href="#" className="font-medium">
+                    Blog
+                  </a>
+                  <a href="#" className="font-medium">
+                    Pricing
+                  </a>
+                </div>
+                <div className="mt-6 flex flex-col gap-4">
+                  <Button variant="outline">Sign in</Button>
+                  <Button>Start for free</Button>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export { Navbar };
