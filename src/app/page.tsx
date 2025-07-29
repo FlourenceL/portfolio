9f159f24b1c9eff94079a4e59071cd58e3a844ca
Flourence <renzo.lapore@gmail.com>
2025-07-29 15:41:33 +0800

    added contact-page and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Logos06Page from "@/components/logos-06/logos-06";
 import SkillsDisplay from "@/components/views/landing-page/skills-display";
 import PortfolioProjectStandardGrid from "@/components/views/landing-page/projects-display";
 import Timeline from "@/components/timeline-01/timeline-01";
+import { Contact2 } from "@/components/views/landing-page/contact2";
+import FooterSimple from "@/components/views/landing-page/footer";
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <SkillsDisplay />
       <PortfolioProjectStandardGrid />
       <Timeline />
+      <Contact2 />
+      <FooterSimple />
     </>
   );
 }
