ae7988e783bcfa1d930f6ca7f1900afb6c83d639
Flourence <renzo.lapore@gmail.com>
2025-07-30 11:42:06 +0800

    Added functionality for the navbar

--- src/components/navbar5.tsx
+++ src/components/navbar5.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { MenuIcon } from "lucide-react";
-
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+import PortfolioHeroSplitLayout from "./views/landing-page/hero";
+import Logos06Page from "./views/landing-page/logos-06/logos-06";
+import SkillsDisplay from "./views/landing-page/skills-display";
+import PortfolioProjectStandardGrid from "./views/landing-page/projects-display";
+import Timeline from "./views/timeline-01/timeline-01";
+import { Contact2 } from "./views/landing-page/contact2";
+import FooterSimple from "./views/landing-page/footer";
 import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
@@ -25,186 +25,122 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Link from "next/link";
 
 const Navbar5 = () => {
-  const features = [
-    {
-      title: "Dashboard",
-      description: "Overview of your activity",
-      href: "#",
-    },
-    {
-      title: "Analytics",
-      description: "Track your performance",
-      href: "#",
-    },
-    {
-      title: "Settings",
-      description: "Configure your preferences",
-      href: "#",
-    },
-    {
-      title: "Integrations",
-      description: "Connect with other tools",
-      href: "#",
-    },
-    {
-      title: "Storage",
-      description: "Manage your files",
-      href: "#",
-    },
-    {
-      title: "Support",
-      description: "Get help when needed",
-      href: "#",
-    },
-  ];
-
   return (
-    <section className="py-4">
-      <div className="container">
-        <nav className="flex items-center justify-between">
-          <a
-            href="https://www.shadcnblocks.com"
-            className="flex items-center gap-2"
-          >
-            <img
-              src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-icon.svg"
-              className="max-h-8"
-              alt="Shadcn UI Navbar"
-            />
-            <span className="text-lg font-semibold tracking-tighter">
-              Shadcnblocks.com
-            </span>
-          </a>
-          <NavigationMenu className="hidden lg:block">
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Features</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <div className="grid w-[600px] grid-cols-2 p-3">
-                    {features.map((feature, index) => (
-                      <NavigationMenuLink
-                        href={feature.href}
-                        key={index}
-                        className="rounded-md p-3 transition-colors hover:bg-muted/70"
-                      >
-                        <div key={feature.title}>
-                          <p className="mb-1 font-semibold text-foreground">
-                            {feature.title}
-                          </p>
-                          <p className="text-sm text-muted-foreground">
-                            {feature.description}
-                          </p>
-                        </div>
-                      </NavigationMenuLink>
-                    ))}
-                  </div>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink
-                  href="#"
-                  className={navigationMenuTriggerStyle()}
-                >
-                  Products
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink
-                  href="#"
-                  className={navigationMenuTriggerStyle()}
-                >
-                  Resources
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink
-                  href="#"
-                  className={navigationMenuTriggerStyle()}
-                >
-                  Contact
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-          <div className="hidden items-center gap-4 lg:flex">
-            <Button variant="outline">Sign in</Button>
-            <Button>Start for free</Button>
-          </div>
-          <Sheet>
-            <SheetTrigger asChild className="lg:hidden">
-              <Button variant="outline" size="icon">
-                <MenuIcon className="h-4 w-4" />
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="top" className="max-h-screen overflow-auto">
-              <SheetHeader>
-                <SheetTitle>
-                  <a
-                    href="https://www.shadcnblocks.com"
-                    className="flex items-center gap-2"
+    <>
+      <section className="py-4 sticky top-0 z-50 bg-black shadow-md">
+        <div className="container">
+          <nav className="flex items-center justify-between">
+            <a
+              href="https://www.shadcnblocks.com"
+              className="flex items-center gap-2"
+            >
+              <img
+                src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-icon.svg"
+                className="max-h-8"
+                alt="Navbar"
+              />
+              <span className="text-lg font-semibold tracking-tighter">
+                Shadcnblocks.com
+              </span>
+            </a>
+            <NavigationMenu className="hidden lg:block">
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <Link href="#home" className={navigationMenuTriggerStyle()}>
+                    Home
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="#skills" className={navigationMenuTriggerStyle()}>
+                    Skills
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link
+                    href="#projects"
+                    className={navigationMenuTriggerStyle()}
                   >
-                    <img
-                      src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-icon.svg"
-                      className="max-h-8"
-                      alt="Shadcn UI Navbar"
-                    />
-                    <span className="text-lg font-semibold tracking-tighter">
-                      Shadcnblocks.com
-                    </span>
-                  </a>
-                </SheetTitle>
-              </SheetHeader>
-              <div className="flex flex-col p-4">
-                <Accordion type="single" collapsible className="mt-4 mb-2">
-                  <AccordionItem value="solutions" className="border-none">
-                    <AccordionTrigger className="text-base hover:no-underline">
-                      Features
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      <div className="grid md:grid-cols-2">
-                        {features.map((feature, index) => (
-                          <a
-                            href={feature.href}
-                            key={index}
-                            className="rounded-md p-3 transition-colors hover:bg-muted/70"
-                          >
-                            <div key={feature.title}>
-                              <p className="mb-1 font-semibold text-foreground">
-                                {feature.title}
-                              </p>
-                              <p className="text-sm text-muted-foreground">
-                                {feature.description}
-                              </p>
-                            </div>
-                          </a>
-                        ))}
-                      </div>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-                <div className="flex flex-col gap-6">
-                  <a href="#" className="font-medium">
-                    Templates
-                  </a>
-                  <a href="#" className="font-medium">
-                    Blog
-                  </a>
-                  <a href="#" className="font-medium">
-                    Pricing
-                  </a>
+                    Projects
+                  </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link
+                    href="#experiences"
+                    className={navigationMenuTriggerStyle()}
+                  >
+                    Experiences
+                  </Link>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+            <div className="hidden items-center gap-4 lg:flex"></div>
+            <Sheet>
+              <SheetTrigger asChild className="lg:hidden">
+                <Button variant="outline" size="icon">
+                  <MenuIcon className="h-4 w-4" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="top" className="max-h-screen overflow-auto">
+                <SheetHeader>
+                  <SheetTitle>
+                    <a
+                      href="https://www.shadcnblocks.com"
+                      className="flex items-center gap-2"
+                    >
+                      <img
+                        src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-icon.svg"
+                        className="max-h-8"
+                        alt="Shadcn UI Navbar"
+                      />
+                      <span className="text-lg font-semibold tracking-tighter">
+                        Shadcnblocks.com
+                      </span>
+                    </a>
+                  </SheetTitle>
+                </SheetHeader>
+                <div className="flex flex-col p-4">
+                  <div className="flex flex-col gap-6">
+                    <a href="#" className="font-medium">
+                      Home
+                    </a>
+                    <a href="#" className="font-medium">
+                      Skills
+                    </a>
+                    <a href="#" className="font-medium">
+                      Projects
+                    </a>
+                    <a href="#" className="font-medium">
+                      Experiences
+                    </a>
+                  </div>
+                  <div className="mt-6 flex flex-col gap-4"></div>
                 </div>
-                <div className="mt-6 flex flex-col gap-4">
-                  <Button variant="outline">Sign in</Button>
-                  <Button>Start for free</Button>
-                </div>
-              </div>
-            </SheetContent>
-          </Sheet>
-        </nav>
-      </div>
-    </section>
+              </SheetContent>
+            </Sheet>
+          </nav>
+        </div>
+      </section>
+      <section id="home">
+        <PortfolioHeroSplitLayout />
+      </section>
+      <section id="skills">
+        <Logos06Page />
+      </section>
+      <SkillsDisplay />
+      <section id="projects">
+        <PortfolioProjectStandardGrid />
+      </section>
+      <section id="experiences">
+        <Timeline />
+      </section>
+      <section id="contact">
+        <Contact2 />
+      </section>
+      <FooterSimple />
+    </>
   );
 };
 
